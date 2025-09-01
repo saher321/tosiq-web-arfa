47047da04ed5a218f3e25f94fb27c3abdffc7b38
Muhammad Talha <mtalha0018@gmail.com>
2025-09-01 10:41:20 +0500

    Js basic if else

---
+++ webpages/assets/js/home.js
@@ -0,0 +1,35 @@
+//***** DATA TYPES */
+// numbers: 1234, 2.54, 30000000000
+// string: "This is text"
+// array: myarray = ["Apple", "Mango", "Orange", "Grapes"]; // myarray[0]
+// object: user = { name: "Ahmad", email: "a@gmail.com" } // user.email
+// boolean: true, false
+// ***********
+
+var a = 10;
+var b = 20;
+var c = a + b + 3 + (a*b + b);
+console.log(c);
+
+function getResult(){
+    var output  = document.getElementById("output")
+    var units   = document.getElementById("units").value;
+
+    var unitPrice   = 11;
+    var tax         = 255;
+    var fuelTax     = 305;
+
+    if (units <= 200) {
+        output.innerText = units * unitPrice + tax + fuelTax;
+    } else if (units > 200 && units <= 250) {
+        unitPrice = 22;
+        output.innerText = units * unitPrice + tax + fuelTax;
+    } else if (units > 250 && units <= 300) {
+        unitPrice = 44;
+        output.innerText = units * unitPrice + tax + fuelTax;
+    } else {
+        unitPrice = 49;
+        output.innerText = units * unitPrice + tax + fuelTax;        
+    }
+
+}
--- webpages/javascript/script.js
+++ webpages/javascript/script.js
@@ -1,8 +1,27 @@
-var ammarJutt // camel case
-var ammar_jutt // snack case
-// var sntc = "This is js";
+// ******** DATA TYPES
+// string: "Text", "My  name"
+// Numbers: 1234567
+// Float: 12.435
+// array: myarray =
+// ['Mouse', 'USB Drive', 'SSD', 'HDD', 'Kyeboard']
+// myarray[2]
+// Object: myobjct =  { name: 'Alex', contact: '123' }
+// myobjct.name;  myobjct.contact;
+// typeof
+// *******************
+// x = 10; y = 20
+// x + y + 3 * (y + 12)
 var a = 10;
-var b = 5;
-var sum = a+b;
-console.log(sum);
-alert(sum);
+var b = 20;
+var c = a / b;
+// console.log(c);
+document.getElementById("result").innerText = c;
+
+/// ******
+
+var x = 10;
+var y = 20;
+var output = x + y + 3 * (y + 12);
+document.getElementById("output").innerText = "Result: " + output;
+// x = 5; y = 10
+// 2 - 10 * x + y
