07d233f0313b39b69d52c3567c5a4f8dabf6351d
Muhammad Talha <mtalha0018@gmail.com>
2025-09-03 11:49:23 +0500

    js nested table

--- webpages/assets/js/nestedloop.js
+++ webpages/assets/js/nestedloop.js
@@ -1,12 +1,25 @@
-var a = 3
-var b = 5;
+var a = 10;
+var b = 10;
 printTables(a, b);
 function printTables(a, b){
     var tableBox = document.getElementById("table-list");
 
     for(var i = 1; i <= a; i++){
-        for(var j = 1; j <= b; j++){
-            console.log(i + " * " + j + " = " + i*j)
+        var innerValue = "";
+        for(var j = 1; j <= b; j++){ //
+           innerValue += i + " * " + j + " = " + i*j + "<br>"
         }
+        tableBox.innerHTML += `
+            <div class="col-lg-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">` +
+                        innerValue
+                        + `</h5>
+                    </div>
+                </div>
+            </div>
+        `
     }
+
 }
