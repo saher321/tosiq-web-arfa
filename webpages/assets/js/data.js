6105b8146974e8b7de38c1e275d31e1244e7fb0a
Muhammad Talha <mtalha0018@gmail.com>
2025-09-10 11:39:18 +0500

    array map

--- webpages/assets/js/data.js
+++ webpages/assets/js/data.js
@@ -10,8 +10,8 @@ let txt = "This is my last class";
 let arr = ["Asad", "Umar", "John Samith", "Sam"];
 // result = arr.splice((arr.length-1), 1, "Lahore", "Karachi");
 let arr2 = ["Lahore", "Karachi"];
-result = [...arr, ...arr2];
-console.log(result)
+// result = [...arr, ...arr2];
+// console.log(result)
 // console.log(result, arr)
 const users = [
     {id: 1, name: "Adil", role: "admin"},
@@ -21,9 +21,12 @@ const users = [
     {id: 5, name: "Saad", role: "user"},
     {id: 6, name: "John Snow", role: "vendor"},
 ];
-
 // result = users.filter(check); // returns a matching data
 // result = users.find(check); // returns a top value 
+let array1 = [2,3,4,5,6,7,8,9];
+result = array1.map((element) => element*2 )
+// let check = (users) => {
+console.log(array1, result);
 
 function check(users){
     if (users.role == "vendor") {
