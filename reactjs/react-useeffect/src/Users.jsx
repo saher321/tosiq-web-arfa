ea25341e4d06519624078769367203fbc03f180f
Muhammad Talha <mtalha0018@gmail.com>
2025-10-01 11:24:24 +0500

    toggle view

--- reactjs/react-useeffect/src/Users.jsx
+++ reactjs/react-useeffect/src/Users.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
 import UserCard from "./components/UserCard";
 import { MoonLoader } from 'react-spinners';
+import { IoGridOutline } from "react-icons/io5";
+import { TiThMenuOutline } from "react-icons/ti";
 
 const Users = () => {
   const [userList, setUserList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isGrid, setIsGrid] = useState(false)
 
   useEffect(() => {
     const getaUser = async () => {
@@ -25,6 +28,18 @@ const Users = () => {
   return (
     <div className="m-4">
       <h1 className="text-3xl">Users</h1>
+      <div className="flex justify-end">
+        <div>
+            <button 
+            className="cursor-pointer shadow bg-white p-2 rounded-lg text-purple-500"
+            onClick={()=>setIsGrid(!isGrid)}>
+                {isGrid ? 
+                <TiThMenuOutline />: 
+                <IoGridOutline />
+                }
+            </button>
+        </div>
+      </div>
       <div>
         {isLoading ? (
           <div className="mt-4">
@@ -36,7 +51,7 @@ const Users = () => {
               <div className="mt-3 grid grid-cols-12 gap-3">
                 {userList.map((user, i) => {
                   return (
-                    <div key={i} className="col-span-4">
+                    <div key={i} className={`${isGrid ? "lg:col-span-4 md:col-span-6 sm:col-span-6" : "col-span-12" }`}>
                       <UserCard user={user} />
                     </div>
                   );
