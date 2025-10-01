d3d853a16257707a9c6dd6109a700b6fa9acb970
Muhammad Talha <mtalha0018@gmail.com>
2025-10-01 10:55:32 +0500

    User data

--- reactjs/react-useeffect/src/Users.jsx
+++ reactjs/react-useeffect/src/Users.jsx
@@ -1,35 +1,55 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import UserCard from "./components/UserCard";
+import { MoonLoader } from 'react-spinners';
 
 const Users = () => {
-    const [userList, setUserList] = useState([]);
+  const [userList, setUserList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(()=>{
-        const getaUser = async () => {
-            const response = await fetch("https://jsonplaceholder.typicode.com/users");
-            const users = await response.json();
-            if(!users){
-                console.log("Users not showing");
-            } else {
-                setUserList(users);
-            }
-        }
-        getaUser();
-    })
-    
+  useEffect(() => {
+    const getaUser = async () => {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const users = await response.json();
+      if (!users) {
+        console.log("Users not showing");
+      } else {
+        setUserList(users);
+        setIsLoading(false);
+      }
+    };
+    getaUser();
+  });
+
   return (
-    <div>
-        <h1>Users</h1>
-            {userList.length > 0 ?
-            <ul>
-                {userList.map((user, i)=>{
-                    return(
-                        <li key={i}>{user.name}</li>
-                    )
+    <div className="m-4">
+      <h1 className="text-3xl">Users</h1>
+      <div>
+        {isLoading ? (
+          <div className="mt-4">
+            <MoonLoader size={60} color="purple" />
+          </div>
+        ) : (
+          <div>
+            {userList.length > 0 ? (
+              <div className="mt-3 grid grid-cols-12 gap-3">
+                {userList.map((user, i) => {
+                  return (
+                    <div key={i} className="col-span-4">
+                      <UserCard user={user} />
+                    </div>
+                  );
                 })}
-            </ul> : "No users were found"
-            }
+              </div>
+            ) : (
+              "No users were found"
+            )}
+          </div>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Users
+export default Users;
