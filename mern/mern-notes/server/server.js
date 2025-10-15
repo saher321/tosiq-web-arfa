76c937a7383f0089e9c0359b16f20c74e9db5e3f
Muhammad Talha <mtalha0018@gmail.com>
2025-10-15 11:43:53 +0500

    Backend: notes:model created db created attach

--- mern/mern-notes/server/server.js
+++ mern/mern-notes/server/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Note from './models/notesModel.js';
 
 dotenv.config();
 const app = express();
@@ -9,6 +10,12 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send("Hello World 1");
 });
+
+app.get('/notes', async (req, res) => {
+    const notes = await Note.find({});
+    return res.send({status: true, notes})
+})
+
 
 app.listen(PORT, () => {
     connectDB();
