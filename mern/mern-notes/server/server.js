5ba6bca7dddc55907924b5ff90eb6ee9b89840d6
Muhammad Talha <mtalha0018@gmail.com>
2025-10-15 11:23:49 +0500

    server changes in dotenv

--- mern/mern-notes/server/server.js
+++ mern/mern-notes/server/server.js
@@ -1,7 +1,9 @@
 //express, dotenv, mongoose, cors
 import express from 'express';
+import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 
+dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
