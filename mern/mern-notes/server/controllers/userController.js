9a645379773a6628ebba824cb91749423c90c2ea
Muhammad Talha <mtalha0018@gmail.com>
2025-10-28 11:50:17 +0500

    Login API

--- mern/mern-notes/server/controllers/userController.js
+++ mern/mern-notes/server/controllers/userController.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcryptjs";
+import jwt from 'jsonwebtoken';
 import User from "../models/userModel.js";
+
 export const register = async (req, res) => {
     const { name, email, password } = req.body;
 
@@ -23,6 +25,39 @@ export const register = async (req, res) => {
             return res.send({status: true, message: "User Registered successfully", user})
         } else {
             return res.send({status: true, message: "Registration failed"})
+        }
+    } catch (error) {
+        console.log(`Error: ${error}`)
+        
+    }
+}
+
+export const login = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({email})
+        if (!user) {
+            return res.send({status: false, message: "User not found with this email"})
+        }
+
+        // match password
+        const isMatched = await bcrypt.compare(password, user.password);
+        // const isMatched = password == user.password;
+
+        if (!isMatched) {
+            return res.send({status: false, message: "Password didn't matched"})
+        }
+        
+        var userToken = jwt.sign({
+            userID: user?._id, 
+            userEmail: user?.email
+        }, process.env.JWT_SECRET, { expiresIn: "1h"});
+
+        if (userToken) {
+            return res.cookie("userToken", userToken).send({status: true, message: "User Loggedin successfully", userToken})
+        } else {
+            return res.send({status: true, message: "Failed to create session"})
         }
     } catch (error) {
         console.log(`Error: ${error}`)
--- mern/mern-notes/server/routes/userRoute.js
+++ mern/mern-notes/server/routes/userRoute.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { register } from '../controllers/userController.js';
+import { login, register } from '../controllers/userController.js';
 
 const userRoute = express.Router();
 
 userRoute.post('/user/register', register)
+userRoute.post('/user/login', login)
 
 export default userRoute;
